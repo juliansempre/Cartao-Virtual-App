32b5dc96d92a4ae6ddfaffaab1dd4796834268cd
Julian <julian_sempre@hotmail.com>
2023-04-06 15:58:02 -0300

    initial commit

--- index.php
+++ index.php
@@ -14,6 +14,7 @@
     <div class='box'>
     <img src="img/logop.png" alt="Aero Copias">
 <p>Atendimento</p>
+<p class="saudamos"></p>
 
   <nav class='icoimg'>
         <img src="img/whatsapp.png" class='iconesimg' alt="whatsapp" onclick="whatAeroporto()">
@@ -40,6 +41,25 @@ Rua Alcides Mourão, 104 - Aroeira
 
     </div>
     <script>
+            var data = new Date();
+    var hora    = data.getHours();  
+
+    if(hora >= 6 && hora <= 11 ) {
+        document.title =' Bom dia';
+        document.querySelector('.saudamos').innerHTML = 'Bom dia';
+    }
+    else if(hora >= 12 && hora <= 17 ) {
+        document.title =' Boa tarde';
+        document.querySelector('.saudamos').innerHTML = 'Boa tarde';
+    }
+    else if(hora >= 18 && hora <= 23) {
+        document.title =' Boa noite';
+        document.querySelector('.saudamos').innerHTML = 'Boa noite';
+    }else if(hora >= 0 && hora <= 5 ) {
+        document.title =' Boa madrugada';
+        document.querySelector('.saudamos').innerHTML = 'Boa madrugada';
+    }
+
         var whatAeroporto =()=>{
             window.open('https://wa.me/message/GMZWBWNO434JN1', '_blank');
         }
